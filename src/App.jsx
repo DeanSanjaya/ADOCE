988bf4555d15dce86fdd0b0adf9995d02c36caa2
Syaeful Annas <syaefulannas33@gmail.com>
2023-12-21 01:03:06 +0700

    add page about & contact

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,20 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 
 function App() {
-	return (
-		<BrowserRouter>
-			<Routes>
-				<Route
-					path="/login"
-					element={<Login />}
-				/>
-				<Route
-					path="/register"
-					element={<Register />}
-				/>
-			</Routes>
-		</BrowserRouter>
-	);
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import MarkunreadIcon from "@mui/icons-material/Markunread";
+import CallIcon from "@mui/icons-material/Call";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+
+const Contact = () => {
+  return (
+    <div className="bg-[#8EACCD] h-screen justify-center flex-col flex px-[75px]">
+      <div className="flex justify-between items-start w-full">
+        <div className="w-2/4">
+          <h1 className="font-bold text-5xl mb-2">Contact US</h1>
+          <div className="bg-white h-1 w-20" />
+          <div className="flex flex-col space-y-10 mt-20">
+            <div className="flex items-center space-x-5">
+              <div className="h-10 w-10 bg-white rounded-full flex justify-center items-center">
+                <MarkunreadIcon />
+              </div>
+              <div className="text-xl font-medium">adoce@gmail.com</div>
+            </div>
+            <div className="flex items-center space-x-5">
+              <div className="h-10 w-10 bg-white rounded-full flex justify-center items-center">
+                <CallIcon />
+              </div>
+              <div className="text-xl font-medium">+62 889-8888-8888</div>
+            </div>
+            <div className="flex items-center space-x-5">
+              <div className="h-10 w-10 bg-white rounded-full flex justify-center items-center">
+                <LocationOnIcon />
+              </div>
+              <div className="text-xl font-medium">Jl. Persil No. 21</div>
+            </div>
+          </div>
+        </div>
+        <div className="w-2/4">
+          <div className="bg-[#337CCF] w-[670px]  rounded-xl flex justify-center">
+            <form action="" className="w-[80%] py-10  flex flex-col">
+              <label htmlFor="" className="font-medium text-xl mb-3">
+                Your Name
+              </label>
+              <input
+                type="text"
+                placeholder="Your name"
+                className="bg-white placeholder:text-slate-300 outline-none rounded-xl w-full py-3 px-4"
+              />
+              <label htmlFor="" className="font-medium text-xl my-3">
+                Phone Number
+              </label>
+              <input
+                type="text"
+                placeholder="Your Phone"
+                className="bg-white placeholder:text-slate-300 outline-none rounded-xl w-full py-3 px-4"
+              />
+              <label htmlFor="" className="font-medium text-xl my-3">
+                Message
+              </label>
+              <input
+                type="text"
+                placeholder="Your Phone"
+                className="bg-white placeholder:text-slate-300 outline-none rounded-xl w-full py-3 px-4 h-32"
+              />
+              <button className="mt-10 w-full bg-[#0766AD] py-3 rounded-xl font-semibold text-white text-xl">
+                Send Message
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
