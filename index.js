81917309af09622e9ecdbb14293d8f21a652e8f2
Nuzlatul Hidayati <nuzhlatul@gmail.com>
2023-12-21 13:42:10 +0700

    about-and-contact

---
+++ index.js
@@ -0,0 +1,12 @@
+// app.js
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello World with React!</h1>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById("app"));
