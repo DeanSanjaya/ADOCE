e33bcdebb06fd8ff3fc6cda5bd05ee76cae47bb6
gizaaditya <gizaaditya1407@gmail.com>
2023-12-26 22:39:03 +0700

    Update Home

--- src/data/comentar.js
+++ src/data/comentar.js
@@ -1,6 +1,7 @@
 import card1 from "../assets/Ellipse 3.png";
 import card2 from "../assets/Ellipse 4.png";
-import card3 from "../assets/Ellipse 6.png";
+import card3 from "../assets/Ellipse 5.png";
+import card4 from "../assets/Ellipse 6.png";
 const comentar = [
   {
     id: 1,
@@ -12,16 +13,23 @@ const comentar = [
   {
     id: 2,
     img: card2,
-    name: "Kevin Slebew",
+    name: "Indah Slebew",
     status: "Pengunjung",
-    comment: "Website mudah di gunakan dan sangat bermanfaat!!!!! ",
+    comment: "Kucingnya Bagus ",
   },
   {
     id: 3,
     img: card3,
-    name: "Kevin Slebew",
+    name: "Bayu Slebew",
     status: "Pengunjung",
-    comment: "Website mudah di gunakan dan sangat bermanfaat!!!!! ",
+    comment: "Adminya sangat ramah dan fast respon ",
+  },
+  {
+    id: 4,
+    img: card4,
+    name: "Shafa Slebew",
+    status: "Pengunjung",
+    comment: "Pilihan Hewannya Banyak dan Lucu-lucu ",
   },
 ];
 
